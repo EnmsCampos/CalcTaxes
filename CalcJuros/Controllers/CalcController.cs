e497b0a92bb9d3c8089c5266fac2d01debdc93c7
Emerson <messon.campos@gmail.com>
2020-10-04 15:46:10 -0300

    Adição do projeto

---
+++ CalcJuros/Controllers/CalcController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalcJuros.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalcController : ControllerBase
+    {
+        private double taxRate = 0.01;
+        private const string GitHubUrl = "https://github.com/EnmsCampos/CalcTaxes";
+
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "Rodando OK" };
+        }
+
+        [HttpGet]
+        [Route("taxaJuros")]
+        public ActionResult<string> TaxaJuros()
+        {
+            return this.taxRate.ToString();
+        }
+
+        [HttpGet]
+        [Route("calculajuros")]
+        public ActionResult<string> Calculajuros([FromQuery] double valorInicial, [FromQuery] int meses)
+        {
+            double valorFinal = 0.0;
+            valorFinal = valorInicial * Math.Pow( (1 + taxRate) , meses);
+            return string.Format("{0:N2}", valorFinal);
+        }
+
+        [HttpGet]
+        [Route("showmethecode ")]
+        public ActionResult<string> ShowmeTheCode()
+        {
+            return GitHubUrl;
+        }
+
+    }
+}
